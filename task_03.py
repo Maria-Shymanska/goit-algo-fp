ff02865c860b0303e209545ea056dfe60d126c81
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-05-25 20:15:27 +0200

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -12,7 +12,7 @@ def dijkstra(graph, start_vertex):
     distances[start_vertex] = 0
     priority_queue = [(0, start_vertex)]
     heapq.heapify(priority_queue)
-    visited = (set)
+    visited = set()
     
     while priority_queue:
         current_distance, current_vertex = heapq.heappop(priority_queue)
@@ -22,4 +22,24 @@ def dijkstra(graph, start_vertex):
         
         visited.add(current_vertex)
         
-        
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+            
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
+
+# Приклад використання
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+start_vertex = 'A'
+distances = dijkstra(graph, start_vertex)
+print(distances)
+      
